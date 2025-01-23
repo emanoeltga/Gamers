8b1b82a35c2c0c2326fa56814930384bc012a370
Emanoel <emanoeltga@gmail.com>
2025-01-22 23:21:06 -0400

    Ajuste na classe GameController

--- src/main/java/com/correia/Gamers/controller/GameController.java
+++ src/main/java/com/correia/Gamers/controller/GameController.java
@@ -1,10 +1,33 @@
 package com.correia.Gamers.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.correia.Gamers.dto.GameDTO;
+import com.correia.Gamers.dto.GameMinDTO;
+import com.correia.Gamers.service.GameService;
+
 @RestController
-@RequestMapping("/games")
+@RequestMapping(value = "/games")
 public class GameController {
 
+	@Autowired
+	private GameService gameService;	
+
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
+
+	@GetMapping
+	public List<GameMinDTO> findAll() {
+		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}
 }
